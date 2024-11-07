47081c1dd34e0546b6c60cb3951457e00ca1e8a9
karthikeyan187463 <L00187463@atu.ie>
2024-11-07 15:23:02 +0000

    Update my_module.py

--- my_module.py
+++ my_module.py
@@ -1,3 +1,8 @@
+"""
+This module contains a simple Python program that prints 'Hello, World!' to the console.
+It serves as a basic example of how to write and run a Python script.
+"""
+
 def main():
     """
     This is a simple Python program that prints 'Hello, World!' to the console.
